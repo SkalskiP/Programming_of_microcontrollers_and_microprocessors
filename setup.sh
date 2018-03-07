882e481add852dcb97281f3a253e24b661d47ae4
SkalskiP <pskalski@student.agh.edu.pl>
2018-03-07 21:53:26 +0100

    setup.sh added

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##################################################################################################################
+# Author 	: 	Piotr Skalski
+# Website 	: 	https://github.com/SkalskiP
+##################################################################################################################
+
+# Getting the latest package lists
+sudo apt-get update
+sudo apt-get install ddd
+
+echo "----------------------"
+echo "Installation completed"
+echo "----------------------"
