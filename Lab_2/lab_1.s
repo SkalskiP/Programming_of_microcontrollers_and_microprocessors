14cd4c65b94b7cdfe5762a6a5d0b7e459a14d8fd
SkalskiP <pskalski@student.agh.edu.pl>
2018-03-08 00:44:49 +0100

    clean up in files from lab 2

--- Lab_2/lab_1.s
+++ Lab_2/lab_1.s
@@ -8,70 +8,62 @@
 #
 #----------------------------------------------------------------
 
-	.equ	write_64, 1	# write data to file function (64bit)
-	.equ	exit_64, 60	# exit program function (64bit)
-	.equ	stdout, 0x01	# handle to stdout
+	.equ	write_64, 1						# write data to file function (64bit)
+	.equ	exit_64, 60						# exit program function (64bit)
+	.equ	stdout, 0x01					# handle to stdout
 	
-	.data
+	.data									# data section
 
-# First sign of first text
-starttxt:			# first message
-	.ascii	"Start/n"
-# First sign of second text
-endtxt:				# second message
-	.ascii	"Finish/n"
-arg1:				# first argument
+starttxt:									# first message (First sign of first text)
+	.ascii	"Start\n"
+endtxt:										# second message (First sign of second text)
+	.ascii	"Finish\n"
+arg1:										# first argument (8 bits long)
 	.byte		1
-arg2:				# second argument
+arg2:										# second argument (16 bits long)
 	.word		2
-arg3:				# third argument
+arg3:										# third argument (32 bits long)
 	.long		3
-result:				# result
+result:										# result (32 bits long)
 	.long		0	
 
-	# Length of first text
-	.equ	startlen, endtxt - starttxt
-	# Length of second text
-	.equ	endlen, arg1 - endtxt
+	.equ	startlen, endtxt - starttxt		# Length of first text
+	.equ	endlen, arg1 - endtxt			# Length of second text
  
-	.text
-	.global _start
+	.text									# text section
+	.global _start							# declares _start function to be global
 	
 _start:
-	# Instruction to move data from one place to other
-	MOV	$write_64,%rax
-	# Instruction to move precisely indicated 8 bits of data
-	# B - 1 bite
-	# W - 2 bites
-	# L - 4 bites
-	# second argument of MOV is register address and we know how big is it
+	MOV		$write_64,%rax 					# Instruction copies the source operand to the destination operand without affecting the source
+											# Instruction to move precisely indicated 8 bits of data B - 1 bite, W - 2 bites, L - 4 bites
 	MOVQ	$stdout,%rdi
-	MOV	$starttxt,%rsi
+	MOV		$starttxt,%rsi
 	MOVQ	$startlen,%rdx
+	
 	# OS function we use 64 bit registers that begin with letter "r"
 	# we need place function arguments in correct registers: rax, rdi, rsi and rdx
 	# at last labs we used registers which names started with "e" they were 32 bits OS
 
 	# rax with size of 64 bits
-	# if rax is to big for us we can use eax with szie of eax
-	# if eax is to gib than we can use ax withsize of 16 bits
-	# ax is made of two smaller registers ah and al, both with szie of 8 bits
+	# if rax is to big for us we can use eax with size of eax
+	# if eax is to big than we can use ax withsize of 16 bits
+	# ax is made of two smaller registers ah and al, both with size of 8 bits
 	
 	SYSCALL
 
-	NOP
-	# Basic aritmetic operations
+	NOP 									# Instruction performs no operation. 
+
 	MOVB	arg1,%al
 	# or we can do MOVL arg1,%eax
 	MOVW	arg2,%bx
 	MOVL	arg3,%ecx
-	# Addition
-	# One of arguments used in addition is replaced with result
-	# Result is located in %eax - that means in place of second argument
-	ADD	%ebx,%eax
-	# Subtraction
-	SUB	%ecx,%eax
-	# Moving result from %eax to register 
+	
+	ADD	%ebx,%eax							# Addition
+											# One of arguments used in addition is replaced with result
+											# Result is located in %eax - that means in place of second argument
+	
+	SUB	%ecx,%eax							# Subtraction
+											# Moving result from %eax to register 
 	MOVL	%eax,result
 
 	NOP
